2514f362095d3e7e247291b2412e019b1c4321b1
AshRepartoFerramenta <dcapod07@hotmail.com>
2020-05-28 10:01:34 +0200

    Correzione Finale

--- js/script.js
+++ js/script.js
@@ -7,16 +7,36 @@
 
 //Poniamo la condizione di ciclo for
 for (var i = 1; i <= 100; i++) {
-// Assegnamo la condizione dei multipli di 3 stampando 'Fizz' come risultato
-  if (i % 3 == 0) {
-    console.log('Fizz');
+// Assegnamo la condizione dei multipli di 5 e 3 stampando 'FizzBuzz' come risultato
+ if (i % 3 == 0 && i % 5 == 0) {
+    console.log('FizzBuzz');
     // Altrimenti per i multipli di 5 stampiamo 'Buzz'
     } else if (i % 5 == 0) {
     console.log('Buzz');
-        // In fine assegnamo la condizione dei multipli di 5 e 3 stampando 'FizzBuzz' come risultato
-        } else if (i % 3 == 0 && i % 5 == 0) {
-        console.log('FizzBuzz');
+        // In fine assegnamo la condizione dei multipli di 3 stampando 'Fizz' come risultato
+        } else if (i % 3 == 0) {
+        console.log('Fizz');
             } else {
             console.log(i);
         }
     }
+
+
+
+
+
+// //Secondo Metodo
+//
+// //Poniamo la condizione di ciclo for
+// for (var i = 1; i <= 100; i++) {
+//   var listaNumeri = document.getElementById('numbers').innerHTML;
+//     }  if (i % 3 == 0) {
+//         onScreen.innerHTML += '<li>' + 'Fizz' + '</li>';
+//             } else if (i % 5 == 0) {
+//                 onScreen.innerHTML += '<li>' + 'Buzz' + '</li>';
+//                 } else if (i % 3 == 0 && i % 5 == 0) {
+//                     onScreen.innerHTML += '<li>' + 'FizzBuzz' + '</li>';
+//                 } else {
+//                     // onScreen.innerHTML += '<li>' + i + '</li>';
+//                 }
+//                 // alert(listaNumeri);
